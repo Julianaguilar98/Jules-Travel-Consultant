c4b539e6e993d7c236521da108383b78a99dbe3c
Julian Aguilar <julianaguilar98@gmail.com>
2021-08-23 02:48:37 -0500

    added price levels and ranking to cards

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -17,6 +17,14 @@ const PlaceDetails = ({ place }) => {
                 />
                 <CardContent>
                     <Typography gutterBottom variant="h5">{ place.name }</Typography>
+                    <Box display="flex" justifyContent="space-between">
+                        <Typography variant="subtitle1">Price</Typography>
+                        <Typography gutterBottom variant="subtitle1">{place.price_level}</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="space-between">
+                        <Typography variant="subtitle1">Ranking</Typography>
+                        <Typography gutterBottom variant="subtitle1">{place.ranking}</Typography>
+                    </Box>
                 </CardContent>
         </Card>
     );
