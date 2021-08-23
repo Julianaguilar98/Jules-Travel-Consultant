beaec22e88b306fa2837b1b72eda2c7d561d6bb5
Julian Aguilar <julianaguilar98@gmail.com>
2021-08-22 20:11:28 -0500

    added styles to all components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import { Autocomplete } from '@react-google-maps/api'
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+import { classes } from 'istanbul-lib-coverage';
+import SearchIcon from '@material-ui/icons/Search';
 
 const Header = () => {
     return(
-        <h1>Header</h1>
+        <AppBar position="static">
+            <Toolbar className={classes.Toolbar}>
+                <Typography variant="h5" className={classes.title}>
+                    Jules Travel Advisor
+                </Typography>
+                <Box display="flex">
+                <Typography variant="h6" className={classes.title}>
+                    Find great food anywhere on the globe!
+                </Typography>
+                    <Autocomplete>
+                        <div className={classes.search}> 
+                            <div className={classes.searchIcon}>
+                                <SearchIcon />
+                            </div>
+                            <InputBase placeholder="Search..." classes={{ root:classes.inputRoot, input: classes.inputInput }}/>
+                        </div>
+                    </Autocomplete>
+                </Box>
+            </Toolbar>
+        </AppBar>
     );
 }
 
