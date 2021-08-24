215656d6f167951a1f8a66f031f3ee9501dad946
Julian Aguilar <julianaguilar98@gmail.com>
2021-08-24 14:27:42 -0500

    added stars and cards to map

--- src/api/index.js
+++ src/api/index.js
@@ -1,40 +1,40 @@
 import axios from 'axios';
 
-export const getPlacesData = async ( type, sw, ne ) => {
-    try {
-        const { data: { data } } = await axios.get('https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary',{
-          params: {
-            bl_latitude: sw.lat,
-            tr_latitude: ne.lat,
-            bl_longitude: sw.lng,
-            tr_longitude: ne.lng
-          },
-          headers: {
-            'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
-            'x-rapidapi-key': '92066643demsh78153fef2b39816p18ac98jsn0832aed0f5a2'
-          }
-        });
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+      params: {
+        bl_latitude: sw.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+        tr_latitude: ne.lat,
+      },
+      headers: {
+        'x-rapidapi-key': process.env.REACT_APP_RAPID_API_TRAVEL_API_KEY,
+        'x-rapidapi-host': 'travel-advisor.p.rapidapi.com'
+      },
+    });
 
-        return data;
-    } catch (error) {
-        console.log(error)
-    }
-
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const getWeatherData = async (lat, lng) => {
-  try{
-    const { data } = await axios.get('https://community-open-weather-map.p.rapidapi.com/find', {
-      params: { lon: lng, lat: lat },
-      headers: {
-        'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
-        'x-rapidapi-key': '92066643demsh78153fef2b39816p18ac98jsn0832aed0f5a2'
-      }
+  try {
+    if (lat && lng) {
+      const { data } = await axios.get('https://community-open-weather-map.p.rapidapi.com/find', {
+        params: { lat, lon: lng },
+        headers: {
+          'x-rapidapi-key': process.env.REACT_APP_RAPID_API_WEATHER_API_KEY,
+          'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com'
+        },
+      });
 
-    });
-    return data;
-  } catch (error){
-    console.log(error)
+      return data;
+    }
+  } catch (error) {
+    console.log(error);
   }
-
-}
+};
