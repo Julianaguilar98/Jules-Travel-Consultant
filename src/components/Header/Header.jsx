d8adacb7b268563559a75da48ae345b5151f7b92
Julian Aguilar <julianaguilar98@gmail.com>
2021-08-24 14:27:58 -0500

    refactored structure

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,45 +1,35 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Autocomplete } from '@react-google-maps/api';
 import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 
 import useStyles from './styles.js';
 
-const Header = ({ setCoordinates }) => {
-    const classes = useStyles();
-    const [autocomplete, setAutocomplete] = useState(null);
+const Header = ({ onPlaceChanged, onLoad }) => {
+  const classes = useStyles();
 
-    const onLoad = (autoC) => setAutocomplete(autoC);
-
-    const onPlaceChanged = () => {
-        const lat = autocomplete.getPlace().geometry.location.lat();
-        const lng = autocomplete.getPlace().geometry.location.lng();
-
-            setCoordinates({ lat, lng });
-    }
-
-    return(
-        <AppBar position="static">
-            <Toolbar className={classes.Toolbar}>
-                <Typography variant="h5" className={classes.title}>
-                    Jules Travel Advisor
-                </Typography>
-                <Box display="flex">
-                <Typography variant="h6" className={classes.title}>
-                    Find great food anywhere on the globe!
-                </Typography>
-                    <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
-                        <div className={classes.search}> 
-                            <div className={classes.searchIcon}>
-                                <SearchIcon />
-                            </div>
-                            <InputBase placeholder="Search..." classes={{ root:classes.inputRoot, input: classes.inputInput }}/>
-                        </div>
-                    </Autocomplete>
-                </Box>
-            </Toolbar>
-        </AppBar>
-    );
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Travel Advisor
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h6" className={classes.title}>
+            Explore new places
+          </Typography>
+          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
+            </div>
+          </Autocomplete>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Header;
